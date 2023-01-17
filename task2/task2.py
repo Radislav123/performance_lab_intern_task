3a0bc21883a5060726b682a9c94d9f68a899f62f
Vlasov R. V <radislavvlasov@gmail.com>
2023-01-17 20:39:04 +0300

    feat: выполнено второе задание

---
+++ task2/task2.py
@@ -0,0 +1,41 @@
+import math
+import sys
+from typing import Iterable, List
+
+
+class Circle:
+    def __init__(self, x: float, y: float, radius: float):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def check(self, x, y) -> int:
+        center_distance = math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+        if center_distance == self.radius:
+            point_result = 0
+        elif center_distance < self.radius:
+            point_result = 1
+        else:
+            point_result = 2
+        return point_result
+
+
+def get_args() -> Iterable:
+    args = [x for x in sys.argv[1:]]
+    return args
+
+
+def calculate() -> List[int]:
+    circle_filepath, points_filepath = get_args()
+    with open(circle_filepath, 'r') as circle_file, open(points_filepath, 'r') as points_file:
+        circle = Circle(*map(float, circle_file.readline().split()), float(circle_file.readline()))
+        results = []
+        for line in points_file:
+            point = [x for x in map(float, line.split())]
+            results.append(circle.check(*point))
+    return results
+
+
+if __name__ == "__main__":
+    for result in calculate():
+        print(result)
